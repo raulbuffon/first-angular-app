a85cb3fe8a5c9ca20f12d9985c723fa78a546ab9
raulbuffon <raulbuffon1999@gmail.com>
2021-06-25 12:36:22 -0300

    Use a function to load data dinamically in template

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ "Title: " + title }}</h2>'
+    template: '<h2>{{ getTitle() }}</h2>'
   })
   export class CoursesComponent{
     title = "List of Courses"
+
+    getTitle() {
+      return this.title;
+    }
   }
