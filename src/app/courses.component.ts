4518c542234b63063fc10407a44ab2630b98c79f
raulbuffon <raulbuffon1999@gmail.com>
2021-06-26 21:37:32 -0300

    A way to convert Dom to Html and sue his properties

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -11,7 +11,13 @@ import { CoursesService } from "./courses.service";
         </li>
       </ul>
       <img src="{{ imgUrlBindBraces }}"/>
-      <img [src]="imgUrlBindParameter"/>
+      <img [src]="imgUrlBindProperty"/>
+
+      <table>
+        <tr>
+          <td [attr.colspan]="colSpan"></td> //here is a way to convert an DOM object to Http and use his properties
+        </tr>
+      </table>
     `
   })
   export class CoursesComponent{
@@ -19,7 +25,9 @@ import { CoursesService } from "./courses.service";
     courses;
 
     imgUrlBindBraces = "http://lorempixel.com/400/200/";
-    imgUrlBindParameter = "http://lorempixel.com/400/200/";
+    imgUrlBindProperty = "http://lorempixel.com/400/200/"; // remember that html and DOM(the rendered page here) can have differences in his properties
+
+    colSpan = 2;
 
     constructor(service: CoursesService) {
       this.courses = service.getCourses();
